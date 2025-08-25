3ce948c711831280f765d3fa8f75dcda560b55ed
ashraful-rahat <spinball61@gmail.com>
2025-08-25 15:45:03 +0600

    server time incerease

--- src/app/db.ts
+++ src/app/db.ts
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 import config from "./config";
- 
 
 declare global {
   var _mongo: { conn: typeof mongoose | null; promise: Promise<typeof mongoose> | null };
@@ -12,20 +11,33 @@ if (!globalAny._mongo) {
   globalAny._mongo = { conn: null, promise: null };
 }
 
-const connectDB = async () => {
+const connectDB = async (retries = 5, delay = 5000) => {
   if (globalAny._mongo.conn) return globalAny._mongo.conn;
 
-  if (!globalAny._mongo.promise) {
-    globalAny._mongo.promise = mongoose.connect(config.database_url, {
-      bufferCommands: false,
-      connectTimeoutMS: 30000,
-      socketTimeoutMS: 45000,
-      serverSelectionTimeoutMS: 30000,
-    });
-  }
+  while (retries > 0) {
+    try {
+      if (!globalAny._mongo.promise) {
+        globalAny._mongo.promise = mongoose.connect(config.database_url, {
+          bufferCommands: false,
+          connectTimeoutMS: 60000, // 60 সেকেন্ড
+          serverSelectionTimeoutMS: 60000, // 60 সেকেন্ড
+          socketTimeoutMS: 45000,
+        });
+      }
 
-  globalAny._mongo.conn = await globalAny._mongo.promise;
-  return globalAny._mongo.conn;
+      globalAny._mongo.conn = await globalAny._mongo.promise;
+      console.log('✅ MongoDB connected successfully!');
+      return globalAny._mongo.conn;
+    } catch (error) {
+      console.error(`Failed to connect to MongoDB. Retries left: ${retries}`);
+      retries--;
+      if (retries === 0) {
+        throw error;
+      }
+      await new Promise(res => setTimeout(res, delay));
+      delay *= 2; //Exponential Backoff
+    }
+  }
 };
 
 export default connectDB;
